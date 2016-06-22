4d6d1abf97c546279f845e80885f695f1d786e91
Sean Wheeler <sewhee@microsoft.com>
2016-06-22 12:39:47 -0700

    refactoring and added logging

--- migrate-scripts/get-brokenlinks.ps1
+++ migrate-scripts/get-brokenlinks.ps1
@@ -1,122 +1,191 @@
-﻿[CmdLetBinding()]
+﻿#requires -Version 2
+[CmdLetBinding()]
 param(
-  [Parameter(Mandatory=$true)][string]$topicRoot,
-  [string]$repoRoot='C:\MyRepos\WindowsServerDocs-pr\WindowsServerDocs'
+  [Parameter(Mandatory = $True)]
+  [ValidateScript({Test-Path $_})]
+  [string]$topicRoot,
+  
+  [Parameter(Mandatory = $True)]
+  [string]$logfile,
+  
+  [ValidateScript({Test-Path $_})]
+  [string]$repoRoot = 'C:\MyRepos\WindowsServerDocs-pr\WindowsServerDocs'
 )
 
-function get-RelativePath {
+function New-Log
+{
+  param(
+    [string]$logfile
+  )
+  New-Item -Path $logfile -Force
+}
+function Write-Log
+{
+  param(
+    [string]$logfile,
+    [string]$message,
+    [switch]$Error
+  )
+  if ($Error) { 
+    Write-Warning -Message $message
+    } else {
+    Write-Output -InputObject $message
+  }
+  Out-File -FilePath $logfile -InputObject $message -Append
+}
+function Get-RelativePath 
+{
   param(
     [string]$fn1,
     [string]$fn2,
     [string]$root
   )
   
-  if ($root.EndsWith("\") -ne $true) {
-    $rootpattern = $root + '\'
-  } else {
+  # Create the regex pattern for the common root path.
+ 
+  if ($root.EndsWith('\')) 
+  {
     $root = $root.Substring(0,$root.Length-1)
   }
-  $rootpattern = $rootpattern -replace '\\','\\'
-  
+  $rootpattern = (Get-Item $root).FullName -replace '\\', '\\'
+ 
+  # Get the file name and its path parts relative to the common root folder 
   $srcFile = Get-ChildItem $fn1
   $targetFile = Get-ChildItem $fn2
 
-  $srcpath = new-object -type psobject -prop @{
-    filename = $srcFile.Name
+  $srcpath = New-Object -TypeName psobject -Property @{
+    filename   = $srcFile.Name
     folderpath = ($srcFile.Directory -replace $rootpattern) -split '\\'
   }
-  if ($srcFile.Directory.FullName -eq $root) { $srcpath.folderpath = '' }
+  if ($srcFile.Directory.FullName -eq $root) 
+  {
+    $srcpath.folderpath = '' 
+  }
 
-  $dstpath = new-object -type psobject -prop @{
-    filename = $targetFile.Name
+  $dstpath = New-Object -TypeName psobject -Property @{
+    filename   = $targetFile.Name
     folderpath = ($targetFile.Directory -replace $rootpattern) -split '\\'
   }
-  if ($tartgetFile.Directory.Fullname -eq $root) { $dstpath.folderpath = '' }
+  if ($tartgetFile.Directory.Fullname -eq $root) 
+  {
+    $dstpath.folderpath = '' 
+  }
   
+  # Get the longest path and iterate over the parts to build the new relative path
   $relsrc = ''
   $reldst = ''
   $count = $srcpath.folderpath.Count
-  if ($count -lt $dstpath.folderpath.Count) {$count = $dstpath.folderpath.Count}
+  if ($count -lt $dstpath.folderpath.Count) 
+  {
+    $count = $dstpath.folderpath.Count
+  }
 
-  for ($x=0; $x -lt $count; $x++) {
-    if ($srcpath.folderpath[$x] -ne $dstpath.folderpath[$x]) {
-      if ($srcpath.folderpath[$x]) {$relsrc += '../'}
-      if ($dstpath.folderpath[$x]) {$reldst += ('{0}/' -f $dstpath.folderpath[$x])}
+  for ($x = 0; $x -lt $count; $x++) 
+  {
+    if ($srcpath.folderpath[$x] -ne $dstpath.folderpath[$x]) 
+    {
+      if ($srcpath.folderpath[$x]) 
+      {
+        $relsrc += '../'
+      }
+      if ($dstpath.folderpath[$x]) 
+      {
+        $reldst += ('{0}/' -f $dstpath.folderpath[$x])
+      }
     }
   }
 
   $newpath = '{0}{1}{2}' -f $relsrc, $reldst, $dstpath.filename
-  Write-Output $newpath
+  Write-Output -InputObject $newpath
 }
-
-function get-filelist 
+function Get-FileList 
 {
   param([string]$filespec, [string]$root)
   $hash = @{}
  
-  Get-ChildItem -Path $root -include $filespec -Recurse | ForEach-Object -Process {
-    $linkpath = $_.fullname
-    $filename = $_.name
+  $EAPref = $ErrorActionPreference
+  $ErrorActionPreference = 'SilentlyContinue'
+
+  Get-ChildItem -Path $root -Include $filespec -Recurse | ForEach-Object -Process {
+    if ($_.PSIsContainer -ne $True) 
+    {
+      $linkpath = $_.fullname
+      $filename = $_.name
     
-    $EAPref = $ErrorActionPreference
-    $ErrorActionPreference = 'SilentlyContinue'
-    try 
-    {
-      $hash.Add($filename,$linkpath)
-    }
-    catch 
-    {
-      Log-Output $logfile "ERROR: Duplicate file name: $linkpath"
-      Log-Output $logfile ('- Duplicate of {0}' -f $hash[$filename])
+      try 
+      {
+        $hash.Add($filename,$linkpath)
+      }
+      catch 
+      {
+        Write-Log $logfile "ERROR: Duplicate file name: $linkpath" -Error
+        Write-Log $logfile ('- Duplicate of {0}' -f $hash[$filename]) -Error
+      }
     }
   }
   $ErrorActionPreference = $EAPref
   Write-Output -InputObject $hash   
 }
-
-function get-links {
+function Get-Links 
+{
   param(
     [string]$filepath
   )
   $linkpattern = '(?<link>!?\[(?<label>[^\]]*)\]\((?<file>[^)#]*)?(?<anchor>#.+)?\))'
   $mdtext = Select-String -Path $filepath -Pattern $linkpattern
-  $mdtext | %{
-    if ($_ -match $linkpattern) {
-      $Matches
+  $mdtext | ForEach-Object -Process {
+    if ($_ -match $linkpattern) 
+    {
+      Write-Output $Matches
     }
   }
 }
 
-$filelist = get-filelist '*' $repoRoot
+New-Log $logfile
 
-#Get-ChildItem -Path $topicRoot -Filter *.md -Exclude TOC.md -Recurse | %{
-Get-ChildItem -Path $topicRoot -Filter *.md -Recurse | %{
+
+
+
+Write-Log $logfile "Creating list of all files in $repoRoot"
+$filelist = Get-FileList -filespec '*' -root $repoRoot
+
+Get-ChildItem -Path $topicRoot -Filter *.md -Recurse | ForEach-Object -Process {
   $fn1 = $_.FullName
   $fn2 = ''
-  '-------------------'
-  $fn1
-  $links = get-links $fn1
-  foreach ($link in $links) {
+  
+  Write-Log $logfile '-------------------'
+  Write-Log $logfile $fn1
+  
+  $links = Get-Links -filepath $fn1
+  foreach ($link in $links) 
+  {
     if (
       ($link['file'].startswith('http') -ne $true) -and 
       ($link['file'].startswith('assetId:') -ne $true) -and 
       ($link['file'] -ne '') 
-    ){
+    )
+    {
       $linkedfile = ($link['file'] -split '/')[-1]
       $fn2 = $filelist[$linkedfile]
-      if ($fn2) { 
-        $newpath = get-RelativePath -fn1 $fn1 -fn2 $fn2 -root $repoRoot
-        try { 
-          $newlink = $link['link'] -replace $link['file'],$newpath
-          if ($link['file'] -ne $newpath) {
-            "New Link: $newlink" 
+      if ($fn2) 
+      { 
+        $newpath = Get-RelativePath -fn1 $fn1 -fn2 $fn2 -root $repoRoot
+        try 
+        { 
+          $newlink = $link['link'] -replace $link['file'], $newpath
+          if ($link['file'] -ne $newpath) 
+          {
+            Write-Log $logfile "New Link: $newlink"
           }
         }
-        catch {
-          Write-Warning ("Invalid link: '{0}'" -f $link['file'])
+        catch 
+        {
+          Write-Log $logfile ("Invalid link: '{0}'" -f $link['file']) -Error
         }
-      } else {
-        Write-Warning "Missing file: $linkedfile"
+      }
+      else 
+      {
+        Write-Log $logfile "Missing file: $linkedfile" -Error
       }
     }
   }
