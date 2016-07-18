ac6050f9dbffcd1059d1017cd4b0b0f0cb91bdc8
Sean Wheeler <sewhee@microsoft.com>
2016-07-18 15:18:59 -0700

    updated linkcoverage report

--- analysis-tools/get-linkcoverage.ps1
+++ analysis-tools/get-linkcoverage.ps1
@@ -1,41 +1,43 @@
 #requires -Version 3
 
-$articlepattern = '[^!]\[(?<label>(?<!\!INCLUDE\[)([^\[\]]*))\]\(((?<file>[^)#]*)?)((?<bkmk>#[^)]*)?)\)'
-  $hash = new-object -type psobject -prop @{
-    link = ""
-    linktype = ""
-    file = ""
-    fullpath = ""
-    fulllink = ""
-  }
+$linkpattern = '(?<mdlink>\[(?<text>[^\]]*)\]\((?<link>(?<path>[^\)#]*)?(?<bkmk>#[^\)\s]*)?)\s*(?<title>[^\)]*)?\))'
+$simplepattern = '\[[^\]]*\]\([^\)]*\)'
 
 dir *.md -rec | ForEach-Object {
   $filename = $_.name
   $filepath = $_.fullname
-  $hash.link = $filename
+
+    $hash = new-object -type psobject -prop ([ordered]@{
+      linkfile = ""
+      linkpath = ""
+      linktype = ""
+      srcfile  = ""
+      fullpath = ""
+      label    = ""
+      bookmark = ""
+      title    = ""
+    })
+
+  $hash.linkfile = $filename
   $hash.linktype = "self"
-  $hash.file = $filename
+  $hash.srcfile  = $filename
   $hash.fullpath = $filepath
   $hash
-  if (select-string -path $filepath -Pattern $articlepattern) {
-    Get-Content $filepath | ForEach-Object {
-      if ($_ -match $articlepattern) {
-        foreach ($m in $matches) {
-          $target = ($m.file -split "/")[-1]
-          if ($target.EndsWith('.md')) {
-            $hash.link = $target
-            if ($filename -eq "TOC.md") {
-              $hash.linktype = "TOC"
-            } else {
-              $hash.linktype = "topic"
-            }
-            $hash.file = $filename
-            $hash.fullpath = $filepath
-            $hash.fulllink = $m.file
-            $hash
-          }
-        }
-      }
+
+  $finds = (select-string -path $filepath -Pattern $simplepattern -AllMatches).Matches
+  foreach ($find in $finds.Value) {
+    if ($find -match $linkpattern) {
+      $target = ($Matches["path"] -split "/")[-1]
+      $hash.linkfile = $target
+      $hash.linkpath = $Matches["path"]
+      $hash.linktype = "topic"
+      if ($filename -eq "TOC.md") { $hash.linktype = "TOC" }
+      $hash.srcfile  = $filename
+      $hash.fullpath = $filepath
+      $hash.label    = $Matches["text"]
+      $hash.bookmark = $Matches["bkmk"]
+      $hash.title    = $Matches["title"]
+      $hash
     }
   }
 }
