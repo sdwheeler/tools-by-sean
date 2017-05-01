c483f5c7f3c0a1ff2fec32ff45f0a8fe336c906d
Sean Wheeler <sewhee@microsoft.com>
2017-05-01 13:59:55 -0700

    updated rules and added scripts

--- migrate-scripts/mapping-scripts/CreateMonikerMapFromModules.ps1
+++ migrate-scripts/mapping-scripts/CreateMonikerMapFromModules.ps1
@@ -18,3 +18,25 @@ foreach ($version in $versions) {
   $map.Add("azurermps-$version",@{'azure-docs-powershell/azureps-cmdlets-docs/ResourceManager'=$modMap})
 }
 $map | ConvertTo-Json -Depth 4 | Out-File $jsonFile -Force -Encoding ascii
+
+$versions = '3.8.0','2.1.0','1.7.0'
+$jsonFile = ".\monikerMapping-azuresmps.json"
+$map = [ordered]@{}
+
+foreach ($version in $versions) {
+  $modMap = @{}
+
+  $modules = dir c:\temp\psgallery\azure-$version -inc Azure.psd1,AzureRM.Profile.psd1,Azure.Storage.psd1 -rec |
+               %{ get-module $_ -list } 
+
+  $modules | %{
+    if (!$modMap.ContainsKey($_.Name)) {
+      $modMap.Add($_.Name,([ordered]@{_displayName=$_.name;_version=$_.version.tostring()}))
+    } else {
+      $modMap[$_.Name] = [ordered]@{_displayName=$_.name;_version=$_.version.tostring()}
+    }
+  }
+
+  $map.Add("azuresmps-$version",@{'azure-docs-powershell/azureps-cmdlets-docs/ResourceManager'=$modMap})
+}
+$map | ConvertTo-Json -Depth 4 | Out-File $jsonFile -Force -Encoding ascii
