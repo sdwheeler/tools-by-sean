31b4c1ad9796446ad6659cf3040e3c07c814ebb3
Sean Wheeler <sean.wheeler@microsoft.com>
2021-08-10 14:13:35 -0500

    save-history

--- modules/sdwheeler.utilities/psutils.psm1
+++ modules/sdwheeler.utilities/psutils.psm1
@@ -5,63 +5,74 @@ function Get-EnumValues {
     ForEach-Object { $enumValues.add($_, $_.value__) }
     $enumValues
   }
-  function Get-Constructors ([type]$type)
-  {
-      foreach ($constr in $type.GetConstructors())
-      {
-          $params = ''
-          foreach ($parameter in $constr.GetParameters())
-          {
-              if ($params -eq '') {
-                  $params =  "{0} {1}" -f $parameter.parametertype.fullname,
-                      $parameter.name
-              } else {
-                $params +=  ", {0} {1}" -f $parameter.parametertype.fullname,
+function Get-Constructors ([type]$type)
+{
+    foreach ($constr in $type.GetConstructors())
+    {
+        $params = ''
+        foreach ($parameter in $constr.GetParameters())
+        {
+            if ($params -eq '') {
+                $params =  "{0} {1}" -f $parameter.parametertype.fullname,
                     $parameter.name
-              }
-          }
-          Write-Host $($constr.DeclaringType.Name) "($params)"
-      }
-  }
-  function normalizeFilename {
-    param([string]$inputString)
-
-    $i = ([IO.Path]::GetInvalidFileNameChars()+'(',')','[',']',' ' | %{ [regex]::Escape($_) }) -join '|'
-
-    $normal = $inputString -replace $i,'-'
-    while ($normal -match '--') {
-      $normal = $normal -replace '--','-'
+            } else {
+              $params +=  ", {0} {1}" -f $parameter.parametertype.fullname,
+                  $parameter.name
+            }
+        }
+        Write-Host $($constr.DeclaringType.Name) "($params)"
     }
-    $normal
+}
+function normalizeFilename {
+  param([string]$inputString)
+
+  $i = ([IO.Path]::GetInvalidFileNameChars()+'(',')','[',']',' ' | %{ [regex]::Escape($_) }) -join '|'
+
+  $normal = $inputString -replace $i,'-'
+  while ($normal -match '--') {
+    $normal = $normal -replace '--','-'
   }
-  #-------------------------------------------------------
-  function kill-module {
-    param(
-      [Parameter(Mandatory=$true)]
-      [string]$module,
+  $normal
+}
+#-------------------------------------------------------
+function kill-module {
+  param(
+    [Parameter(Mandatory=$true)]
+    [string]$module,
 
-      [Parameter(Mandatory=$true)]
-      [string]$version,
+    [Parameter(Mandatory=$true)]
+    [string]$version,
 
-      [switch]$Force
-    )
-    'Creating list of dependencies...'
-    $depmods = Find-Module $module -RequiredVersion $version | select -exp dependencies |
-        select @{l='name';e={$_.name}},@{l='ver';e={$_.requiredversion}}
+    [switch]$Force
+  )
+  'Creating list of dependencies...'
+  $depmods = Find-Module $module -RequiredVersion $version | select -exp dependencies |
+      select @{l='name';e={$_.name}},@{l='ver';e={$_.requiredversion}}
 
-    $depmods += @{name=$module; version=$version}
+  $depmods += @{name=$module; version=$version}
 
-    $saveErrorPreference =  $ErrorActionPreference
-    $ErrorActionPreference = 'SilentlyContinue'
+  $saveErrorPreference =  $ErrorActionPreference
+  $ErrorActionPreference = 'SilentlyContinue'
 
-    foreach ($mod in $depmods) {
-      'Uninstalling {0}' -f $mod.name
-      try {
-        uninstall-module $mod.name -RequiredVersion $mod.ver -Force:$Force -ErrorAction Stop
-      } catch {
-        write-host ("`t" + $_.FullyQualifiedErrorId)
-      }
+  foreach ($mod in $depmods) {
+    'Uninstalling {0}' -f $mod.name
+    try {
+      uninstall-module $mod.name -RequiredVersion $mod.ver -Force:$Force -ErrorAction Stop
+    } catch {
+      write-host ("`t" + $_.FullyQualifiedErrorId)
     }
-
-    $ErrorActionPreference = $saveErrorPreference
   }
+
+  $ErrorActionPreference = $saveErrorPreference
+}
+#-------------------------------------------------------
+function save-history {
+  $date = Get-Date -f 'yyyy-MM-dd'
+  $oldlog = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt"
+  $newlog = "C:\Users\sewhee\Documents\PowerShell\History\ConsoleHost_history_$date.txt"
+  copy $oldlog $newlog -Force
+  Get-History |
+    Select-Object -ExpandProperty CommandLine |
+    Sort-Object -Unique |
+    Out-File $oldlog -Force
+}
