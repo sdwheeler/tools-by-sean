8283a84833210e3f96b670bbfebccb191f59d42e
Sean Wheeler <sean.wheeler@microsoft.com>
2021-09-09 09:21:31 -0500

    add Get-LocaleFreshness

--- modules/sdwheeler.utilities/contenttools.psm1
+++ modules/sdwheeler.utilities/contenttools.psm1
@@ -547,82 +547,96 @@ Set-Alias -Name ww -Value Swap-WordWrapSettings
 function Sort-Parameters {
   [CmdletBinding()]
   param (
-      [Parameter()]
-      [SupportsWildcards()]
-      [string[]]
-      $path
+    [Parameter()]
+    [SupportsWildcards()]
+    [string[]]
+    $path
   )
 
   # ----------------------
   function findparams {
-      param($matchlist)
+    param($matchlist)
 
-      $paramlist = @()
+    $paramlist = @()
 
-      $inParams = $false
-      foreach ($hdr in $matchlist) {
-          if ($hdr.Line -eq '## Parameters') {
-              $inParams = $true
-          }
-          if ($inParams) {
-              if ($hdr.Line -match '^### -') {
-                  $param = [PSCustomObject]@{
-                      Name      = $hdr.Line.Trim()
-                      StartLine = $hdr.LineNumber - 1
-                      EndLine   = -1
-                  }
-                  $paramlist += $param
-              }
-              if ((
-                   ($hdr.Line -match '^## ' -and $hdr.Line -ne '## Parameters') -or
-                   ($hdr.Line -eq '### CommonParameters')
-                  ) -and
-                  ($paramlist.Count -gt 0)
-              ) {
-                  $inParams = $false
-                  $paramlist[-1].EndLine = $hdr.LineNumber - 2
-              }
-          }
+    $inParams = $false
+    foreach ($hdr in $matchlist) {
+      if ($hdr.Line -eq '## Parameters') {
+        $inParams = $true
       }
-      if ($paramlist.Count -gt 0) {
-          for ($x = 0; $x -lt $paramlist.Count; $x++) {
-              if ($paramlist[$x].EndLine -eq -1) {
-                  $paramlist[$x].EndLine = $paramlist[($x + 1)].StartLine - 1
-              }
+      if ($inParams) {
+        if ($hdr.Line -match '^### -') {
+          $param = [PSCustomObject]@{
+            Name      = $hdr.Line.Trim()
+            StartLine = $hdr.LineNumber - 1
+            EndLine   = -1
           }
+          $paramlist += $param
+        }
+        if ((
+            ($hdr.Line -match '^## ' -and $hdr.Line -ne '## Parameters') -or
+            ($hdr.Line -eq '### CommonParameters')
+          ) -and
+          ($paramlist.Count -gt 0)
+        ) {
+          $inParams = $false
+          $paramlist[-1].EndLine = $hdr.LineNumber - 2
+        }
       }
-      $paramlist
+    }
+    if ($paramlist.Count -gt 0) {
+      for ($x = 0; $x -lt $paramlist.Count; $x++) {
+        if ($paramlist[$x].EndLine -eq -1) {
+          $paramlist[$x].EndLine = $paramlist[($x + 1)].StartLine - 1
+        }
+      }
+    }
+    $paramlist
   }
   # ----------------------
 
   $mdfiles = Get-ChildItem $path
 
   foreach ($file in $mdfiles) {
-      $file.Name
-      $mdtext = Get-Content $file -Encoding utf8
-      $mdheaders = Select-String -Pattern '^#' -Path $file
+    $file.Name
+    $mdtext = Get-Content $file -Encoding utf8
+    $mdheaders = Select-String -Pattern '^#' -Path $file
 
-      $unsorted = findparams $mdheaders
-      if ($unsorted.Count -gt 0) {
-          $sorted = $unsorted | Sort-Object Name
-          $newtext = $mdtext[0..($unsorted[0].StartLine - 1)]
-          $confirmWhatIf = @()
-          foreach ($p in $sorted) {
-              if ( '### -Confirm', '### -WhatIf' -notcontains $p.Name) {
-                  $newtext += $mdtext[$p.StartLine..$p.EndLine]
-              }
-              else {
-                  $confirmWhatIf += $p
-              }
-          }
-          foreach ($p in $confirmWhatIf) {
-              $newtext += $mdtext[$p.StartLine..$p.EndLine]
-          }
-          $newtext += $mdtext[($unsorted[-1].EndLine + 1)..($mdtext.Count - 1)]
-
-          Set-Content -Value $newtext -Path $file.FullName -Encoding utf8 -Force
+    $unsorted = findparams $mdheaders
+    if ($unsorted.Count -gt 0) {
+      $sorted = $unsorted | Sort-Object Name
+      $newtext = $mdtext[0..($unsorted[0].StartLine - 1)]
+      $confirmWhatIf = @()
+      foreach ($p in $sorted) {
+        if ( '### -Confirm', '### -WhatIf' -notcontains $p.Name) {
+          $newtext += $mdtext[$p.StartLine..$p.EndLine]
+        }
+        else {
+          $confirmWhatIf += $p
+        }
       }
+      foreach ($p in $confirmWhatIf) {
+        $newtext += $mdtext[$p.StartLine..$p.EndLine]
+      }
+      $newtext += $mdtext[($unsorted[-1].EndLine + 1)..($mdtext.Count - 1)]
+
+      Set-Content -Value $newtext -Path $file.FullName -Encoding utf8 -Force
+    }
   }
+}
+#-------------------------------------------------------
+function Get-LocaleFreshness {
+  param(
+    [uri]$uri
+  )
+
+  $locales = 'en-us', 'cs-cz', 'de-de', 'es-es', 'fr-fr', 'hu-hu', 'it-it', 'ja-jp', 'ko-kr', 'nl-nl',
+             'pl-pl', 'pt-br', 'pt-pt', 'ru-ru', 'sv-se', 'tr-tr', 'zh-cn', 'zh-tw'
+  $locale = $uri.Segments[1].Trim('/')
+  $url = $uri.OriginalString
+  $loc | ForEach-Object { get-metatags ($url -replace $locale, $_) |
+         Select-Object locale, 'ms.contentlocale', 'ms.translationtype', 'ms.date' } |
+         Sort-Object 'ms.date', 'ms.contentlocale'
 }
 #-------------------------------------------------------
 #endregion
