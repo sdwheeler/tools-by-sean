4ed1c283b0bb5aa3b1932765f8c8f613358829e4
Sean Wheeler <sean.wheeler@microsoft.com>
2022-03-04 16:14:52 -0600

    Initial commit

---
+++ modules/WhatsNew/WhatsNew.psm1
@@ -0,0 +1,119 @@
+﻿############################
+#
+# TODO LIST
+# - fix version handling
+# - write code to fixup URLs in docs
+#
+############################
+function Get-WhatsNew {
+    [CmdletBinding(DefaultParameterSetName = 'ByVersion')]
+    param (
+        [Parameter(Position=0,ParameterSetName='ByVersion')]
+        [Parameter(Position=0,ParameterSetName='CompareVersion')]
+        [ValidateSet(5.1,7.0,7.1,7.2,7.3)]
+        [double]$Version,
+        ## TODO: fix 7.0 version handling
+
+        [Parameter(Mandatory,ParameterSetName='CompareVersion')]
+        [ValidateSet(5.1,7.0,7.1,7.2,7.3)]
+        [double]$CompareVersion,
+
+        [Parameter(Mandatory,ParameterSetName='AllVersions')]
+        [switch]$All,
+
+        [Parameter(Position=0,ParameterSetName='ByVersion')]
+        [Alias('MOTD')]
+        [switch]$Daily,
+
+        [Parameter(ParameterSetName='ByVersion')]
+        [switch]$Online
+    )
+
+    $versions = @(
+        ([PSCustomObject]@{
+            version = [version]'5.1'
+            path = 'relnotes/What-s-New-in-Windows-PowerShell-50.md'
+            url = 'https://docs.microsoft.com/powershell/scripting/windows-powershell/whats-new/what-s-new-in-windows-powershell-50'
+        }),
+        ([PSCustomObject]@{
+            version = [version]'7.0'
+            path = 'relnotes/What-s-New-in-PowerShell-70.md'
+            url = 'https://docs.microsoft.com/powershell/scripting/whats-new/what-s-new-in-powershell-70'
+        }),
+        ([PSCustomObject]@{
+            version = [version]'7.1'
+            path = 'relnotes/What-s-New-in-PowerShell-71.md'
+            url = 'https://docs.microsoft.com/powershell/scripting/whats-new/what-s-new-in-powershell-71'
+        }),
+        ([PSCustomObject]@{
+            version = [version]'7.2'
+            path = 'relnotes/What-s-New-in-PowerShell-72.md'
+            url = 'https://docs.microsoft.com/powershell/scripting/whats-new/what-s-new-in-powershell-72'
+        }),
+        ([PSCustomObject]@{
+            version = [version]'7.3'
+            path = 'relnotes/What-s-New-in-PowerShell-73.md'
+            url = 'https://docs.microsoft.com/powershell/scripting/whats-new/what-s-new-in-powershell-73'
+        })
+    )
+
+    if (0 -eq $Version) {
+        $Version = [double]('{0}.{1}' -f$PSVersionTable.PSVersion.Major,$PSVersionTable.PSVersion.Minor)
+    }
+
+    $mdfiles = @()
+    if ($PsCmdlet.ParameterSetName -eq 'CompareVersion') {
+        if ($Version -gt $CompareVersion) {
+            $tempver = $CompareVersion
+            $CompareVersion = $Version
+            $Version = $tempver
+        }
+        foreach ($ver in $versions) {
+            if (($ver.version -ge $Version) -and ($ver.version -le $CompareVersion)) {
+                $mdfiles += $ver.path
+            }
+        }
+    } elseif ($PsCmdlet.ParameterSetName -eq 'AllVersions') {
+        $mdfiles = ($versions).path
+    } else {
+        $mdfiles = ($versions | Where-Object version -eq $Version).path
+    }
+
+    foreach ($file in $mdfiles) {
+        $mdtext = Get-Content $file -Encoding utf8
+        $mdheaders = Select-String -Pattern '^##\s' -Path $file
+
+        $blocklist = @()
+
+        foreach ($hdr in $mdheaders) {
+            $block = [PSCustomObject]@{
+                Name      = $hdr.Line.Trim()
+                StartLine = $hdr.LineNumber - 1
+                EndLine   = -1
+            }
+            $blocklist += $block
+        }
+        if ($blocklist.Count -gt 0) {
+            for ($x = 0; $x -lt $blocklist.Count; $x++) {
+                if ($blocklist[$x].EndLine -eq -1) {
+                    $blocklist[$x].EndLine = $blocklist[($x + 1)].StartLine - 1
+                }
+            }
+        }
+        $blocklist[-1].EndLine = $mdtext.Count - 1
+
+        if ($Daily) {
+            $block = $blocklist | Get-Random -SetSeed (get-date -UFormat '%s')
+            #$mdtext[$block.StartLine]
+            $mdtext[$block.StartLine..$block.EndLine]
+        } elseif ($Online) {
+            Start-Process ($versions | Where-Object version -eq $Version).url
+        } else {
+            foreach ($block in $blocklist) {
+                $mdtext[$block.StartLine..$block.EndLine] # |
+                    # ConvertFrom-Markdown -AsVT100EncodedString |
+                    # Select-Object -ExpandProperty VT100EncodedString
+            }
+        }
+    }
+}
---
+++ modules/WhatsNew/relnotes/What-s-New-in-PowerShell-73.md
@@ -0,0 +1,62 @@
+---
+title: What's New in PowerShell 7.3-preview.1
+description: New features and changes released in PowerShell 7.3-preview.1
+ms.date: 02/09/2022
+---
+
+# What's New in PowerShell 7.3
+
+PowerShell 7.3 is the next preview release, built on .NET 6.0.
+
+PowerShell 7.3-preview.1 includes the following features, updates, and breaking changes.
+
+- Set `$?` correctly for command expression with redirections (#16046)
+- Fix a casting error when using `$PSNativeCommandUseErrorActionPreference` (#15993)
+- Make the native command error handling optionally honor `ErrorActionPreference` (#15897)
+- Fix tab completion within the script block specified for the `ValidateScriptAttribute`. (#14550)
+  (Thanks @MartinGC94!)
+- Add `-HttpVersion` parameter to web cmdlets (#15853) (Thanks @hayhay27!)
+- Add support to web cmdlets for open-ended input tags (#16193) (Thanks @farmerau!)
+- Fix `ConvertTo-Json -Depth` to allow 100 at maximum (#16197) (Thanks @KevRitchie!)
+- Specify the executable path as `TargetObject` for non-zero exit code ErrorRecord (#16108) (Thanks
+  @rkeithhill!)
+- Invoke-Command: improve handling of variables with $using: expression (#16113) (Thanks @dwtaber!)
+- Add `clean` block to script block as a peer to `begin`, `process`, and `end` to allow easy
+  resource cleanup (#15177)
+
+For a complete list of changes, see the [Change Log][CHANGELOG] in the GitHub repository.
+
+## Experimental Features
+
+> [!NOTE]
+> There is a known issue in 7.3.0-preview.1 - Alpine Linux packages are missing the
+> `powershell.config.json` file, causing experimental features to be disabled by default. For
+> details, see Issue #16636.
+
+PowerShell 7.3 introduces the following experimental features:
+
+- [PSExec][exp-psexec] - Adds the new `Switch-Process` cmdlet (alias `exec`) to provide `exec`
+  compatibility for non-Windows systems.
+- [PSCleanBlock][exp-clean] - Adds `clean` block to script block as a peer to `begin`, `process`,
+  and `end` to allow easy resource cleanup.
+- [PSStrictModeAssignment][exp-strict] - Adds the **StrictMode** parameter to `Invoke-Command` to
+  allow specifying strict mode when invoking command locally.
+- [PSNativeCommandErrorActionPreference][exp-error] - Adds the
+  `$PSNativeCommandUseErrorActionPreference` variable to enable errors produced by native commands
+  to be PowerShell errors.
+
+For more information about the Experimental Features, see [Using Experimental Features][exp].
+
+## Breaking Changes and Improvements
+
+- Add `clean` block to script block as a peer to `begin`, `process`, and `end` to allow easy
+  resource cleanup (#15177)
+- Change default for `$PSStyle.OutputRendering` to **Ansi**
+
+<!-- reference links -->
+
+[CHANGELOG]: https://github.com/PowerShell/PowerShell/releases/tag/v7.3.0-preview.1
+[exp-clean]: ../learn/experimental-features.md#pscleanblock
+[exp-psexec]: ../learn/experimental-features.md#psexec
+[exp-strict]: ../learn/experimental-features.md#psstrictmodeassignment
+[exp-error]: ../learn/experimental-features.md#psnativecommanderroractionpreference
