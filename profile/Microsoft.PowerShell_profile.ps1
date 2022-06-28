d4be893cbc8c4ece33b02598c117d6880bb245ab
Sean Wheeler <sean.wheeler@microsoft.com>
2022-06-28 16:28:17 -0500

    remove Kusto pipes

--- profile/Microsoft.PowerShell_profile.ps1
+++ profile/Microsoft.PowerShell_profile.ps1
@@ -241,10 +241,10 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
     $Local:word = $wordToComplete.Replace('"', '""')
     $Local:ast = $commandAst.ToString().Replace('"', '""')
-    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition
-    | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-    }
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
 }
 #endregion
 
