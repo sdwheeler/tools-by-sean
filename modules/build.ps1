e1341294370f0753818b822e45c1cb836cac70ba
Sean Wheeler <sean.wheeler@microsoft.com>
2023-08-24 20:32:45 -0500

    Add build script

---
+++ modules/build.ps1
@@ -0,0 +1,31 @@
+param(
+    [string[]]$Module = 'sdwheeler.*',
+    [switch]$clean,
+    [switch]$publish
+)
+if ( $clean ) {
+    $null = if (Test-Path "${PSScriptRoot}/out") {
+        Remove-Item "${PSScriptRoot}/out" -Recurse -Force
+    }
+}
+
+if ($publish) {
+    $ModuleList = (dir $Module -dir).BaseName
+    foreach ($moduleName in $ModuleList) {
+        $psd = Import-PowerShellDataFile "$PSScriptRoot/${moduleName}/${moduleName}.psd1"
+        $moduleVersion = $psd.ModuleVersion
+        $moduleDeploymentDir = "${PSScriptRoot}/out/${moduleName}/${moduleVersion}"
+
+        # create directory with version
+        # copy files to that location
+        if (-not (Test-Path $moduleDeploymentDir)) {
+            New-Item -Type Directory -Force -Path $moduleDeploymentDir
+        }
+        Copy-Item -Force -Recurse "$PSScriptRoot/${moduleName}/*" $moduleDeploymentDir
+
+        if (-not (test-path $moduleDeploymentDir)) {
+            throw "Could not find '$moduleDeploymentDir'"
+        }
+        Publish-PSResource -Path $moduleDeploymentDir -Repository LocalPSResource
+    }
+}
