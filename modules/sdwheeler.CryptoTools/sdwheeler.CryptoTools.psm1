0371fcbaf30fd445b6e69c666c75f7a58ef780b3
Sean Wheeler <sean.wheeler@microsoft.com>
2024-06-06 09:01:20 -0500

    Update Show-Certificate

--- modules/sdwheeler.CryptoTools/sdwheeler.CryptoTools.psm1
+++ modules/sdwheeler.CryptoTools/sdwheeler.CryptoTools.psm1
@@ -15,25 +15,47 @@ function Get-Hash {
 }
 #-------------------------------------------------------
 function Show-Certificate {
+    [CmdletBinding(DefaultParameterSetName='ByPath')]
     param(
-        [string[]]$Certificate,
+        [Parameter(ValueFromPipelineByPropertyName, Position = 0, ParameterSetName='ByPath')]
+        [Alias('PSPath')]
+        [string[]]$Path,
+
+        [Parameter(ValueFromPipeline, Position = 0, ParameterSetName='ByCert')]
+        [System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate,
+
         [switch]$Multiline
     )
+    begin {
+        function SelectProperties {
+            param(
+                [System.Security.Cryptography.X509Certificates.X509Certificate2]$Certificate
+            )
 
-    foreach ($certpath in $certificate) {
-        Get-Item $certpath | Select-Object -Property Subject, DNSNameList, NotBefore, NotAfter,
-        Issuer, PolicyId, Archived, FriendlyName, SerialNumber, Thumbprint, HasPrivateKey,
-        @{ N = 'SignatureAlgorithm';  E = { $_.SignatureAlgorithm.FriendlyName } },
-        @{ N = 'CertTemplateInfo';    E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Certificate Template Information' }).Format($multiline) } },
-        @{ N = 'KeyUsage';            E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Key Usage' }).Format($multiline) } },
-        @{ N = 'EnhKeyUsage';         E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Enhanced Key Usage' }).Format($multiline) } },
-        @{ N = 'AppPolicies';         E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Application Policies' }).Format($multiline) } },
-        @{ N = 'SubjectKeyId';        E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Subject Key Identifier' }).Format($multiline) } },
-        @{ N = 'AuthorityKeyId';      E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Authority Key Identifier' }).Format($multiline) } },
-        @{ N = 'CRLDistPoints';       E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'CRL Distribution Points' }).Format($multiline) } },
-        @{ N = 'AuthorityInfoAccess'; E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Authority Information Access' }).Format($multiline) } },
-        @{ N = 'SubjectAltName';      E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Subject Alternative Name' }).Format($multiline) } },
-        @{ N = 'BasicConstraints';    E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Basic Constraints' }).Format($multiline) } }
+            $Certificate | Select-Object -Property Subject, DNSNameList, NotBefore, NotAfter,
+            Issuer, PolicyId, Archived, FriendlyName, SerialNumber, Thumbprint, HasPrivateKey,
+            @{ N = 'SignatureAlgorithm';  E = { $_.SignatureAlgorithm.FriendlyName } },
+            @{ N = 'CertTemplateInfo';    E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Certificate Template Information' }).Format($multiline) } },
+            @{ N = 'KeyUsage';            E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Key Usage' }).Format($multiline) } },
+            @{ N = 'EnhKeyUsage';         E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Enhanced Key Usage' }).Format($multiline) } },
+            @{ N = 'AppPolicies';         E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Application Policies' }).Format($multiline) } },
+            @{ N = 'SubjectKeyId';        E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Subject Key Identifier' }).Format($multiline) } },
+            @{ N = 'AuthorityKeyId';      E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Authority Key Identifier' }).Format($multiline) } },
+            @{ N = 'CRLDistPoints';       E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'CRL Distribution Points' }).Format($multiline) } },
+            @{ N = 'AuthorityInfoAccess'; E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Authority Information Access' }).Format($multiline) } },
+            @{ N = 'SubjectAltName';      E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Subject Alternative Name' }).Format($multiline) } },
+            @{ N = 'BasicConstraints';    E = { ($_.Extensions | Where-Object { $_.Oid.FriendlyName -eq 'Basic Constraints' }).Format($multiline) } }
+        }
+    }
+
+    process {
+        if ($PSCmdlet.ParameterSetName -eq 'ByPath') {
+            foreach ($certpath in $Path) {
+                SelectProperties (Get-Item $certpath)
+            }
+        } else {
+            SelectProperties $Certificate
+        }
     }
 }
 #endregion
