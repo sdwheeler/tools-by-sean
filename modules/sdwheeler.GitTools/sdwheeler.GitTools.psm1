8a96f3963e1012c3934aa88999d73d85a0f4fd27
Sean Wheeler <sean.wheeler@microsoft.com>
2026-07-01 12:53:37 -0500

    Add parameter validation for Scope

--- modules/sdwheeler.GitTools/sdwheeler.GitTools.psm1
+++ modules/sdwheeler.GitTools/sdwheeler.GitTools.psm1
@@ -276,7 +276,14 @@ function Connect-GitHub {
         [string]$ClientId = $env:GITHUB_OAUTH_CLIENT_ID,
 
         [Parameter()]
-        [string]$Scope = 'repo read:org'
+        [ValidateSet('repo', 'repo:status', 'repo_deployment', 'public_repo', 'repo:invite',
+            'security_events', 'admin:repo_hook', 'write:repo_hook', 'read:repo_hook', 'admin:org',
+            'write:org', 'read:org', 'admin:public_key', 'write:public_key', 'read:public_key',
+            'admin:org_hook', 'gist', 'notifications', 'user', 'read:user', 'user:email',
+            'user:follow', 'project', 'read:project', 'delete_repo', 'write:packages',
+            'read:packages', 'delete:packages', 'admin:gpg_key', 'write:gpg_key', 'read:gpg_key',
+            'codespace', 'workflow')]
+        [string[]]$Scope = 'repo read:org'
     )
 
     if ([string]::IsNullOrEmpty($ClientId)) {
@@ -289,7 +296,7 @@ function Connect-GitHub {
         Method = 'POST'
         Body = @{
             client_id = $ClientId
-            scope     = $Scope
+            scope     = ($Scope -join ' ')
         }
         ContentType = 'application/x-www-form-urlencoded'
         Headers = @{
